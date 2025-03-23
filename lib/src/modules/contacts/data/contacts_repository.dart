09dbdaec6d5a1842ebc9f3a7f20815260c914250
Jefferson Rodrigues <jefferson.david@equipeblue.com.br>
2025-03-23 17:36:37 -0300

    criado repositorio

--- lib/src/modules/contacts/data/contacts_repository.dart
+++ lib/src/modules/contacts/data/contacts_repository.dart
@@ -1,7 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:minha_agenda/src/models/contato_model.dart';
 import 'package:minha_agenda/src/modules/contacts/data/contacts_datasource.dart';
+import 'package:minha_agenda/src/utils/app_failures.dart';
 
 class ContactsRepository {
   final ContactsDatasource datasource;
 
   ContactsRepository({required this.datasource});
+
+  Future<Either<String, List<ContatoModel>>> buscarTodosOsContatos(
+    String userId,
+  ) async {
+    try {} on DBFailure catch (e) {
+      return Left(e.message);
+    }
+  }
 }
