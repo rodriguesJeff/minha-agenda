af8cb6d892ff75bb9309d6ea326f3bbb7ff3835c
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2025-03-23 01:09:54 -0300

    feat: inicio do modulo de contato

---
+++ lib/src/modules/contacts/data/contacts_repository.dart
@@ -0,0 +1,7 @@
+import 'package:minha_agenda/src/modules/contacts/data/contacts_datasource.dart';
+
+class ContactsRepository {
+  final ContactsDatasource datasource;
+
+  ContactsRepository({required this.datasource});
+}
